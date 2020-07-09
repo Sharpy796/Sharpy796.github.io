7a35b359e66bbc33944595bb1ae88c125ef3fb31
Ubuntu <ubuntu@ip-172-31-55-188.ec2.internal>
2020-07-09 20:50:58 +0000

    I hope this works...

---
+++ projects/runtime/js/level01.js
@@ -0,0 +1,177 @@
+var level01 = function (window) {
+
+    window.opspark = window.opspark || {};
+
+    var draw = window.opspark.draw;
+    var createjs = window.createjs;
+
+    window.opspark.runLevelInGame = function(game) {
+        // some useful constants 
+        var groundY = game.groundY;
+
+        // this data will allow us to define all of the
+        // behavior of our game
+        var levelData = {
+            "name": "Robot Romp",
+            "number": 1, 
+            "speed": -3,
+            "gameItems": [  
+                { "type": "spikeBall" , "x": 400 , "y": groundY - 25  },
+                { "type": "goomba"    , "x": 450 , "y": groundY - 23  },
+                { "type": "spikeBall" , "x": 650 , "y": groundY - 100 },
+                { "type": "spikeBall" , "x": 900 , "y": groundY - 25  },
+                { "type": "mushroom"  , "x": 925 , "y": groundY - 130 },
+                { "type": "spikeBall" , "x": 950 , "y": groundY - 25  },
+                { "type": "goomba"    , "x": 1000, "y": groundY - 23  },
+                { "type": "mushroom"  , "x": 1125, "y": groundY - 130 },
+                { "type": "bossGoomba", "x": 1250, "y": groundY - 48  },
+                { "type": "spikeBall" , "x": 1500, "y": groundY - 25  },
+                { "type": "spikeBall" , "x": 1700, "y": groundY - 25  },
+                { "type": "spikeBall" , "x": 2000, "y": groundY - 100 },
+                { "type": "star"      , "x": 2500, "y": groundY - 150 },
+            ]
+        };
+        window.levelData = levelData;
+        // set this to true or false depending on if you want to see hitzones
+        game.setDebugMode(false);
+
+        // BEGIN EDITING YOUR CODE HERE
+        function createSpikeBall(x, y) {
+            var hitZoneSize = 25;
+            var damageFromObstacle = 10;
+            var spikeBallHitZone = game.createObstacle(hitZoneSize, damageFromObstacle);
+            spikeBallHitZone.x = x;
+            spikeBallHitZone.y = y;
+            spikeBallHitZone.rotationalVelocity = -20;
+            game.addGameItem(spikeBallHitZone);
+            var obstacleImage = draw.bitmap('img/SpikeBall.png');
+            spikeBallHitZone.addChild(obstacleImage);
+            obstacleImage.x = -25;
+            obstacleImage.y = -25;
+        }
+        
+        function createMushroom(x, y) {
+            var mushroom = game.createGameItem('enemy',25);
+            var mushroomImage = draw.bitmap('img/SuperMush.png');
+            mushroomImage.x = -25;
+            mushroomImage.y = -25;
+            mushroom.addChild(mushroomImage);
+            mushroom.x = x;
+            mushroom.y = y;
+            game.addGameItem(mushroom);
+            mushroom.velocityX = -2;
+            mushroom.onPlayerCollision = function() {
+                console.log('Halle has collected a Mushroom!');
+                game.changeIntegrity(20);
+                game.increaseScore(200)
+                mushroom.fadeOut();
+            };
+            mushroom.onProjectileCollision = function() {}
+        }
+        
+        function createGoomba(x, y) {
+            var goomba = game.createGameItem('enemy',25);
+            var goombaImage = draw.bitmap('img/Goomba.gif');
+            goombaImage.x = -25;
+            goombaImage.y = -25;
+            goomba.addChild(goombaImage);
+            goomba.x = x;
+            goomba.y = y;
+            game.addGameItem(goomba);
+            goomba.velocityX = -1;
+            goomba.rotationalVelocity = 10;
+            goomba.onPlayerCollision = function() {
+                console.log('The Goomba has hit Halle!');
+                game.changeIntegrity(-40);
+            };
+            goomba.onProjectileCollision = function() {
+                console.log('Halle has hit the Goomba!');
+                game.increaseScore(500);
+                goomba.flyTo(1000, 0);
+            }
+        }
+        
+        function createBossGoomba(x, y) {
+            var bossGoomba = game.createGameItem('enemy',25);
+            var bossGoombaImage = draw.bitmap('img/Goomba.gif');
+            bossGoombaImage.x = -25;
+            bossGoombaImage.y = -25;
+            bossGoomba.addChild(bossGoombaImage);
+            bossGoomba.x = x;
+            bossGoomba.y = y;
+            bossGoomba.scaleX = 2;
+            bossGoomba.scaleY = 2;
+            game.addGameItem(bossGoomba);
+            bossGoomba.velocityX = -1;
+            bossGoomba.rotationalVelocity = 5;
+            bossGoomba.onPlayerCollision = function() {
+                console.log('The Boss Goomba has hit Halle!');
+                game.changeIntegrity(-80);
+            };
+            bossGoomba.onProjectileCollision = function() {
+                console.log('Halle has defeated the Boss Goomba!');
+                game.increaseScore(500);
+                bossGoomba.flyTo(1000, 0);
+            }
+        }
+        
+        function createStar(x, y) {
+            var star = game.createGameItem('enemy',25);
+            var starImage = draw.bitmap('img/SuperStar.png');
+            starImage.x = -25;
+            starImage.y = -25;
+            star.addChild(starImage);
+            star.x = x;
+            star.y = y;
+            star.scaleX = 1.5;
+            star.scaleY = 1.5;
+            game.addGameItem(star);
+            star.velocityX = -2;
+            star.onPlayerCollision = function() {
+                console.log('Halle has collected the Super Star!');
+                console.log('You won!');
+                game.increaseScore(10000)
+                star.shrink();
+            };
+            star.onProjectileCollision = function() {}
+        }
+        
+        function createError(x, y) {
+            var error = game.createGameItem('enemy',25);
+            var errorImage = draw.bitmap('img/Error.png');
+            errorImage.x = -25;
+            errorImage.y = -25;
+            error.addChild(errorImage);
+            error.x = x;
+            error.y = y;
+            game.addGameItem(error);
+        }
+        
+        for (var i = 0; i < levelData.gameItems.length; i++) {
+            var gameItem = levelData.gameItems[i];
+            if (gameItem.type === "spikeBall") {
+                createSpikeBall(gameItem.x, gameItem.y);
+            } else if (gameItem.type === "mushroom") {
+                createMushroom(gameItem.x, gameItem.y);
+            } else if (gameItem.type === "goomba") {
+                createGoomba(gameItem.x, gameItem.y);
+            } else if (gameItem.type === "bossGoomba") {
+                createBossGoomba(gameItem.x, gameItem.y);
+            } else if (gameItem.type === "star") {
+                createStar(gameItem.x, gameItem.y);
+            } else {
+                createError(gameItem.x, gameItem.y);
+            }
+        }
+        
+        
+        // DO NOT EDIT CODE BELOW HERE
+    };
+};
+
+// DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
+if((typeof process !== 'undefined') &&
+    (typeof process.versions.node !== 'undefined')) {
+    // here, export any references you need for tests //
+    module.exports = level01;
+}
---
+++ projects/runtime/js/view/background.js
@@ -0,0 +1,156 @@
+var background = function (window) {
+    'use strict';
+    
+    window.opspark = window.opspark || {};
+    var draw = window.opspark.draw;
+    var createjs = window.createjs;
+    
+    /*
+     * Create a background view for our game application
+     */
+    window.opspark.makeBackground = function(app,ground) {
+        /* Error Checking - DO NOT DELETE */
+        if(!app) {
+            throw new Error("Invaid app argument");
+        }
+        if(!ground || typeof(ground.y) == 'undefined') {
+            throw new Error("Invalid ground argument");
+        }
+        
+        // useful variables
+        var canvasWidth = app.canvas.width;
+        var canvasHeight = app.canvas.height;
+        var groundY = ground.y;
+        
+        // container which will be returned
+        var background;
+        
+        // ANIMATION VARIABLES HERE:
+        var trees = [];
+        
+        var buildings = [];
+
+        
+        // called at the start of game and whenever the page is resized
+        // add objects for display in background. draws each image added to the background once
+        function render() {
+            background.removeAllChildren();
+            
+            // TODO: 2 - Part 2
+            // this fills the background with a obnoxious yellow
+            // you should modify this to suit your game
+            var backgroundFill = draw.rect(canvasWidth,groundY,'black');
+            background.addChild(backgroundFill);
+            
+            // TODO: 3 - Add a moon and starfield
+            var circle;
+            for(var i=0;i<1000;i++) {
+                var divideFive = i % 5;
+                if (divideFive === 0) {
+                    circle = draw.circle(0.5,'white','Pink',2);
+                } else if (divideFive === 1) {
+                    circle = draw.circle(0.5,'white','LightBlue',2);
+                } else if (divideFive === 2) {
+                    circle = draw.circle(0.5,'white','Yellow',2);
+                } else if (divideFive === 3) {
+                    circle = draw.circle(0.5,'white','LightGreen',2);
+                }
+                circle.x = canvasWidth*Math.random();
+                circle.y = groundY*Math.random();
+                background.addChild(circle);
+            }
+            
+            var moon = draw.bitmap('img/moon.png');
+            moon.x = 1100;
+            moon.y = 50;
+            moon.scaleX = 0.25;
+            moon.scaleY = 0.25;
+            background.addChild(moon);
+            
+            
+            // TODO: 5 - Add buildings!     Q: This is before TODO 4 for a reason! Why?
+            for (var i = 0; i < 8; i++) {
+                var divideFive = i % 5;
+                var buildingHeight;
+                if (divideFive === 1) {
+                    buildingHeight = 100
+                } else if (divideFive === 2) {
+                    buildingHeight = 300
+                } else if (divideFive === 3) {
+                    buildingHeight = 200
+                } else if (divideFive === 4) {
+                    buildingHeight = 250;
+                } else {
+                    buildingHeight = 150;
+                }
+                var building = draw.rect(75, buildingHeight, 'DarkBlue', 'Blue', 5);
+                building.x = 200 * i;
+                building.y = groundY - buildingHeight - 3;
+                background.addChild(building);
+                buildings.push(building);
+            }
+            
+            // TODO 4: Part 1 - Add a tree
+            for (var i = -1; i < 7; i++) {
+                var tree = draw.bitmap('img/otherTree.png');
+                tree.x = i * 200;
+                tree.y = groundY - 210;
+                background.addChild(tree);
+                trees.push(tree);
+            }
+            
+            
+        } // end of render function - DO NOT DELETE
+        
+        
+        // Perform background animation
+        // called on each timer "tick" - 60 times per second
+        function update() {
+            // useful variables
+            var canvasWidth = app.canvas.width;
+            var canvasHeight = app.canvas.height;
+            var groundY = ground.y;
+            
+            // TODO 4: Part 2 - Move the tree!
+            for (var i = 0; i < trees.length; i++) {
+                var tree = trees[i];
+                tree.x -= 1.5;
+                if (tree.x < -225) {
+                    tree.x = canvasWidth;
+                }
+            }
+            
+            // TODO 5: Part 2 - Parallax
+            for (var i = 0; i < buildings.length; i++) {
+                var building = buildings[i];
+                building.x -= 0.5;
+                if (building.x < -225) {
+                    building.x = canvasWidth;
+                }
+            }
+            
+        } // end of update function - DO NOT DELETE
+        
+        
+        
+        /* Make a createjs Container for the background and let it know about the render and upate functions*/
+        background = new createjs.Container();
+        background.resize = render;
+        background.update = update;
+        
+        /* make the background able to respond to resizing and timer updates*/
+        app.addResizeable(background);
+        app.addUpdateable(background);
+        
+        /* render and return the background */
+        render();
+        return background;
+    };
+};
+
+// DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
+if((typeof process !== 'undefined') &&
+    (typeof process.versions.node !== 'undefined')) {
+    // here, export any references you need for tests //
+    module.exports = background;
+}
