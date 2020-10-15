b71b5d8633d413c084c1a60bb2684aab43f5c80d
ChristianBankovic <chribankovic@tangistudents.org>
2020-10-15 23:51:06 +0000

    Added a pause and cheat mode!

--- pong/index.js
+++ pong/index.js
@@ -9,32 +9,40 @@ function runProgram() {
 
     // Constant Variables
     var FRAMES_PER_SECOND_INTERVAL = 1000 / 60;
+    var BORDERS = {
+        TOP: 0,
+        LEFT: 0,
+        BOTTOM: $("#board").width(),
+        RIGHT: ($("#board").height() - $("#ball").width()),
+    }
     var KEY = {
         /* general controls */
-        ENTER: 16,
-        P: 80,
-        R: 82,
+        ENTER: 16,  // ???
+        P: 80,      // pause
+        R: 82,      // restart
 
         /* P1 controls */
-        W: 87,
-        A: 65,
-        S: 83,
-        D: 68,
+        W: 87,      // up
+        A: 65,      // left
+        S: 83,      // down
+        D: 68,      // right
 
         /* P2 controls */
-        UP: 38,
-        LEFT: 37,
-        DOWN: 40,
-        RIGHT: 39,
+        UP: 38,     // up
+        LEFT: 37,   // left
+        DOWN: 40,   // down
+        RIGHT: 39,  // right
     }
 
     // Game Item Objects
 
-    var paddleLeft = createGameObject(50, 180, 0, 0, "#paddleLeft");
+    var paddleLeft = createGameObject(50, 180, 0, 0, "#paddleLeft");    // player 1
 
-    var paddleRight = createGameObject(630, 180, 0, 0, "#paddleRight");
+    var paddleRight = createGameObject(630, 180, 0, 0, "#paddleRight"); // player 2
 
-    var ball = createGameObject(340, 210, 5, 5, "#ball");
+    var ball = createGameObject(340, 210, 5, 5, "#ball");               // ball
+
+    var pause = createGameObject(10, 10, 0, 0, "#cheatIcon");           // cheat icon
 
     var score = {
         p1: 0,
@@ -55,11 +63,17 @@ function runProgram() {
         error: "ERROR",
     }
 
+    var isPaused = false;
+    var pIsDown = false;
+
 
     // one-time setup
     var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-    $(document).on("keydown", handleKeyDown);   
-    $(document).on("keyup", handleKeyUp);
+    $(document).on("keydown", handleKeyDown);   // listen for keydown events
+    $(document).on("keyup", handleKeyUp);       // listen for keyup events
+    $("#cheatIcon").on("click", activateCheatMode);       // listen for click events
+    $("#cheatIcon").hide();
+
 
     ////////////////////////////////////////////////////////////////////////////////
     ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -70,13 +84,13 @@ function runProgram() {
     by calling this function and executing the code inside.
     */
     function newFrame() {
-        repositionGameItem(paddleLeft);
-        repositionGameItem(paddleRight);
-        // moveGameItem(ball);
+        pauseGame();
+        if (isPaused) {
+        } else {
+            repositionAllGameItems();
+        }
 
-        redrawGameItem("#paddleLeft", paddleLeft);
-        redrawGameItem("#paddleRight", paddleRight);
-        redrawGameItem("#ball", ball);
+        redrawAllGameItems();
     }
 
     /* 
@@ -87,37 +101,38 @@ function runProgram() {
         console.log(keycode);
 
         /* general controls */
-        if (keycode === KEY.ENTER) {
+        if (keycode === KEY.ENTER) {        // ???
             console.log("enter pressed");
-        } if (keycode === KEY.P) {
+        } if (keycode === KEY.P) {          // pause
+            pIsDown = true;
             console.log("p pressed");
-        } if (keycode === KEY.R) {
+        } if (keycode === KEY.R) {          // restart
             console.log("r pressed");
         }
 
         /* P1 controls */
-        if (keycode === KEY.W) {
+        if (keycode === KEY.W) {            // up
             paddleLeft.speedY = -5;
             console.log("w pressed");
-        } if (keycode === KEY.A) {
+        } if (keycode === KEY.A) {          // left
             console.log("a pressed");
-        } if (keycode === KEY.S) {
+        } if (keycode === KEY.S) {          // down
             paddleLeft.speedY = 5;
             console.log("s pressed");
-        } if (keycode === KEY.D) {
+        } if (keycode === KEY.D) {          // right
             console.log("d pressed");
         }
 
         /* P2 controls */
-        if (keycode === KEY.UP) {
+        if (keycode === KEY.UP) {           // up
             paddleRight.speedY = -5;
             console.log("up pressed");
-        } if (keycode === KEY.LEFT) {
+        } if (keycode === KEY.LEFT) {       // left
             console.log("left pressed");
-        } if (keycode === KEY.DOWN) {
+        } if (keycode === KEY.DOWN) {       // down
             paddleRight.speedY = 5;
             console.log("down pressed");
-        } if (keycode === KEY.RIGHT) {
+        } if (keycode === KEY.RIGHT) {      // right
             console.log("right pressed");
         }
     }
@@ -130,6 +145,7 @@ function runProgram() {
         if (keycode === KEY.ENTER) {
             console.log("enter released");
         } if (keycode === KEY.P) {
+            pIsDown = false;
             console.log("p released");
         } if (keycode === KEY.R) {
             console.log("r released");
@@ -170,14 +186,6 @@ function runProgram() {
     ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
     ////////////////////////////////////////////////////////////////////////////////
 
-    function endGame() {
-        // stop the interval timer
-        clearInterval(interval);
-
-        // turn off event handlers
-        $(document).off();
-    }
-
     function createGameObject(x, y, speedX, speedY, id) {
         var gameObject = {};
         gameObject.x = x;
@@ -196,5 +204,56 @@ function runProgram() {
     function redrawGameItem(id, gameItem) {
         $(id).css("left", gameItem.x);
         $(id).css("top", gameItem.y);
+    }
+
+    function repositionAllGameItems() {
+        repositionGameItem(paddleLeft);
+        repositionGameItem(paddleRight);
+        repositionGameItem(ball);
+    }
+
+    function redrawAllGameItems() {
+        redrawGameItem("#paddleLeft", paddleLeft);
+        redrawGameItem("#paddleRight", paddleRight);
+        redrawGameItem("#ball", ball);
+    }
+    
+    var i = 1;
+    function pauseGame() {
+        if (pIsDown) {
+            if (i < 2) {
+                if (isPaused) {
+                    isPaused = false;
+                    $("#ball").css("background-color", "fuchsia");
+                    $("#cheatIcon").hide();
+                    console.log("unpause");
+                } else {
+                    isPaused = true;
+                    $("#ball").css("background-color", "lime");
+                    $("#cheatIcon").show();
+                    console.log("pause");
+                }
+            }
+            i += 1;
+        } else {
+            i = 1;
+        }
+    }
+
+    function activateCheatMode() {
+        var answer = prompt("Password:");
+        if (answer === "^^vv<><>ba") {
+            alert("Cheat Mode Activated!");
+        } else {
+            alert("Wrong Password.");
+        }
+    }
+
+    function endGame() {
+        // stop the interval timer
+        clearInterval(interval);
+
+        // turn off event handlers
+        $(document).off();
     }
 }
