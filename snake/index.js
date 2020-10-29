e1fbd939636a5beee1e4b7874107aa7f876e6452
ChristianBankovic <chribankovic@tangistudents.org>
2020-10-29 23:36:59 +0000

    stuff

--- snake/index.js
+++ snake/index.js
@@ -1,53 +1,116 @@
 /* global $, sessionStorage */
 
 $(document).ready(runProgram); // wait for the HTML / CSS elements of the page to fully load, then execute runProgram()
-  
-function runProgram(){
-  ////////////////////////////////////////////////////////////////////////////////
-  //////////////////////////// SETUP /////////////////////////////////////////////
-  ////////////////////////////////////////////////////////////////////////////////
 
-  // Constant Variables
-  var FRAMES_PER_SECOND_INTERVAL = 1000 / 60;
-  
-  // Game Item Objects
+function runProgram() {
+    ////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////// SETUP /////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////
+
+    // Constant Variables
+    var FRAMES_PER_SECOND_INTERVAL = 1000 / 60;
+    var BORDERS = {
+        TOP: 0,
+        LEFT: 0,
+        BOTTOM: $("#board").height(),
+        RIGHT: $("#board").width(),
+    }
+    var KEY = {
+        UP: 38,     // up
+        LEFT: 37,   // left
+        DOWN: 40,   // down
+        RIGHT: 39,  // right
+    }
+
+    // Game Item Objects
+
+    var head = createGameObject(1, 1, 0, 0, 0, '#head');
+    // var tail = createGameObject(2, 1, 0, 0, 0, '#tail');
+
+    var apple = createGameObject(5, 5, 0, 0, 0, '#apple');
+
+    var snakeArray = [];
+    snakeArray[0] = head;
+    // snakeArray[snakeArray.length - 1] = tail;
 
 
-  // one-time setup
-  var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
+    // one-time setup
+    var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
+    $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
 
-  ////////////////////////////////////////////////////////////////////////////////
-  ///////////////////////// CORE LOGIC ///////////////////////////////////////////
-  ////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////
+    ///////////////////////// CORE LOGIC ///////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////
 
-  /* 
-  On each "tick" of the timer, a new frame is dynamically drawn using JavaScript
-  by calling this function and executing the code inside.
-  */
-  function newFrame() {
+    /* 
+    On each "tick" of the timer, a new frame is dynamically drawn using JavaScript
+    by calling this function and executing the code inside.
+    */
+    function newFrame() {
+        repositionAllGameItems();
+        redrawAllGameItems();
+
+    }
+
+    /* 
+    Called in response to events.
+    */
+    function handleKeyDown(event) {
+        var keycode = event.which;
+        console.log(keycode);
+
+        if (keycode === KEY.UP) {           // up
+            console.log("up pressed");
+        } if (keycode === KEY.LEFT) {       // left
+            console.log("left pressed");
+        } if (keycode === KEY.DOWN) {       // down
+            console.log("down pressed");
+        } if (keycode === KEY.RIGHT) {      // right
+            console.log("right pressed");
+        }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////
+
+    function createGameObject(column, row, speedX, speedY, score, id) {
+        var gameObject = {};
+        gameObject.column = column;
+        gameObject.row = row;
+        gameObject.x = 20 * column;
+        gameObject.y = 20 * row;
+        gameObject.speedX = speedX;
+        gameObject.speedY = speedY;
+        gameObject.score = score;
+        gameObject.id = id;
+        return gameObject;
+    }
     
+    function repositionGameItem(gameItem) {
+        gameItem.x += gameItem.speedX;
+        gameItem.y += gameItem.speedY;
+    }
 
-  }
-  
-  /* 
-  Called in response to events.
-  */
-  function handleEvent(event) {
+    function redrawGameItem(id, gameItem) {
+        $(id).css("left", gameItem.x);
+        $(id).css("top", gameItem.y);
+    }
 
-  }
+    function repositionAllGameItems() {
+        repositionGameItem(head);
+    }
 
-  ////////////////////////////////////////////////////////////////////////////////
-  ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
-  ////////////////////////////////////////////////////////////////////////////////
+    function redrawAllGameItems() {
+        redrawGameItem(head.id, head);
+    }
 
-  
-  function endGame() {
-    // stop the interval timer
-    clearInterval(interval);
+    function endGame() {
+        // stop the interval timer
+        clearInterval(interval);
 
-    // turn off event handlers
-    $(document).off();
-  }
-  
+        // turn off event handlers
+        $(document).off();
+    }
+
 }
